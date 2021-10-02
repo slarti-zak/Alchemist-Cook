5afb7483600caefb1da0ced63d469f6cd2a9f6a4
Bernd Hochschulz <b.hochschulz@mailbox.org>
2021-10-02 21:35:30 +0200

    Add padding on bottom on instruction list

--- app/src/main/java/click/alchemist/cook/ui/recipe/detail/RecipeDetailExtendedInstruction.kt
+++ app/src/main/java/click/alchemist/cook/ui/recipe/detail/RecipeDetailExtendedInstruction.kt
@@ -1,10 +1,12 @@
 package click.alchemist.cook.ui.recipe.detail
 
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import click.alchemist.cook.compose.recipe.RecipeExtendedInstructions
 import click.alchemist.cook.service.markdown.MarkdownService
 import click.alchemist.cook.viewmodel.RecipeGraphModel
@@ -13,5 +15,10 @@ import click.alchemist.cook.viewmodel.RecipeGraphModel
 @ExperimentalMaterialApi
 @Composable
 fun RecipeDetailExtendedInstruction(graph: RecipeGraphModel, markdownService: MarkdownService? = null) {
-	RecipeExtendedInstructions(graph, Modifier.fillMaxSize(), markdownService = markdownService)
+	RecipeExtendedInstructions(
+		graph,
+		Modifier.fillMaxSize(),
+		contentPadding = PaddingValues(8.dp, 8.dp, 8.dp, 100.dp),
+		markdownService = markdownService
+	)
 }
