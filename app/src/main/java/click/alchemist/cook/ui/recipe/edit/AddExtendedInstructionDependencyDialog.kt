6203ecbcf6c302bdfed00f1de1634c32ff119e89
Bernd Hochschulz <b.hochschulz@mailbox.org>
2022-01-09 17:55:06 +0100

    Fix ingredient node dialog cutting off title

--- app/src/main/java/click/alchemist/cook/ui/recipe/edit/AddExtendedInstructionDependencyDialog.kt
+++ app/src/main/java/click/alchemist/cook/ui/recipe/edit/AddExtendedInstructionDependencyDialog.kt
@@ -1,18 +1,20 @@
 package click.alchemist.cook.ui.recipe.edit
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.AlertDialog
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.DialogProperties
 import click.alchemist.cook.R
 import click.alchemist.cook.compose.recipe.RecipeExtendedInstruction
+import click.alchemist.cook.model.RecipeGraphNode
 import click.alchemist.cook.service.markdown.MarkdownService
 import click.alchemist.cook.viewmodel.RecipeGraphNodeModel
 
@@ -24,17 +26,32 @@ fun AddExtendedInstructionDependencyDialog(
 	onSelected: (RecipeGraphNodeModel) -> Unit = {},
 	markdownService: MarkdownService? = null
 ) {
-	AlertDialog(onDismissRequest = onDismiss,
+	AlertDialog(
+		onDismissRequest = onDismiss,
 		confirmButton = {},
 		dismissButton = { TextButton(onClick = onDismiss) { Text(stringResource(R.string.general_cancel)) } },
-		title = { Text("Pick Node") },
+		title = { /* Don't put title here as it gets cut off when the lazy column grows in height  */ },
 		text = {
-			LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp), contentPadding = PaddingValues(8.dp)) {
-				items(dependentNodes, key = { it.node.id }) {
-					RecipeExtendedInstruction(node = it, onClick = { onSelected(it) }, markdownService = markdownService)
+			Column {
+				CompositionLocalProvider(
+					LocalContentAlpha provides ContentAlpha.high,
+					LocalTextStyle provides MaterialTheme.typography.subtitle1
+				) {
+					Text("Pick Node")
+				}
+				LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp), contentPadding = PaddingValues(8.dp)) {
+					items(dependentNodes, key = { it.node.id }) { node ->
+						RecipeExtendedInstruction(node = node, onClick = { onSelected(it) }, markdownService = markdownService)
+					}
 				}
 			}
 		},
 		properties = DialogProperties(usePlatformDefaultWidth = true)
 	)
+}
+
+@Preview
+@Composable
+private fun Preview() {
+	AddExtendedInstructionDependencyDialog(listOf(RecipeGraphNodeModel(RecipeGraphNode(text = "T1"), "")), {})
 }
