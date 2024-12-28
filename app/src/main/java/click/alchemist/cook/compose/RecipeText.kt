089d728e3261c74cc8e06ebac452eb60227677aa
b.hochschulz <b.hochschulz@mailbox.org>
2024-12-28 16:34:51 +0100

    dark mode fix for markdown

--- app/src/main/java/click/alchemist/cook/compose/RecipeText.kt
+++ app/src/main/java/click/alchemist/cook/compose/RecipeText.kt
@@ -1,8 +1,10 @@
 package click.alchemist.cook.compose
 
 import android.util.TypedValue
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
@@ -11,11 +13,13 @@ import click.alchemist.cook.service.markdown.MarkdownService
 @Composable
 fun RecipeText(text: String, modifier: Modifier = Modifier, markdownService: MarkdownService? = null, sp: TextUnit = 12.sp) {
 	val textSize = with(LocalDensity.current) { sp.toPx() }
+	val textColor = MaterialTheme.colorScheme.onSurface.toArgb();
 	MarkdownText(
 		text,
 		modifier,
 		{
 			it.setTextSize(TypedValue.COMPLEX_UNIT_PX, textSize)
+			it.setTextColor(textColor)
 		},
 		markdownService
 	)
